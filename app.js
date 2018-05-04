24681d642f5272b1ad41d10e18f6c4af67390e02
John Wolfe <jwolfe890@gmail.com>
2018-05-04 17:38:34 -0400

    app.js

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=5685%20SE%20Lamay%20Drive',
     json: true 
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
 });
