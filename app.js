b691ea5b2ddc1fcb649405de9efc351fea7dcdb8
John Wolfe <jwolfe890@gmail.com>
2018-05-06 08:31:18 -0400

    added yargs argv config

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const request = require('request');
+const yargs = require('yargs')
+
+const argv = yargs
+    .options({
+       a: {
+           demand: true,
+           alias: 'address',
+           describe: 'Address to fetch weather for',
+           string: true   
+       }
+    }) 
 
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=5685%20SE%20Lamay%20Drive',
     json: true 
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
