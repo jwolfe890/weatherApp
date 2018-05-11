e20ad2d262cfe97e764b253f3c3270f173fa67db
John Wolfe <jwolfe890@gmail.com>
2018-05-11 01:31:43 -0400

    added app2

---
+++ app2.js
@@ -0,0 +1,19 @@
+const yargs = require('yargs');
+const axios = require('axios');
+
+const argv = yargs
+    .options({ a: { demand: true, alias: 'address', describe: 'Address to fetch weather for', string: true } })
+    .help()
+    .alias('help', 'h')
+    .argv;  
+
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
+
+axios.get(geocodeUrl).then((response) => {
+    console.log(response.data);
+}).catch((e) => {
+   if (e.code === 'ENOTFOUND') {
+       console.log('Unable to connect')
+   }
+});
