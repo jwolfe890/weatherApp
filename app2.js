046b841950f411f6d4db856745a00f5eeaea587a
John Wolfe <jwolfe890@gmail.com>
2018-05-11 02:06:58 -0400

    added double axios request

--- app2.js
+++ app2.js
@@ -11,9 +11,25 @@ var encodedAddress = encodeURIComponent(argv.address);
 var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
 
 axios.get(geocodeUrl).then((response) => {
-    console.log(response.data);
+
+    if (response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Unable to find that address.')
+    }
+
+    var latitude = response.data.results[0].geometry.location.lat;
+    var longitude = response.data.results[0].geometry.location.lng;
+    var weatherUrl = `https://api.darksky.net/forecast/e7f5012fb3b8e7d1ad843035fd4df7ad/${latitude},${longitude}`
+
+    console.log(response.data.results[0].formatted_address);
+    return axios.get(weatherUrl);
+}).then((response) => {
+    var temperature = response.data.currently.temperature;
+    var apparentTemperature = response.data.currently.apparentTemperature;
+    console.log(`It's currently ${temperature}.`)
 }).catch((e) => {
-   if (e.code === 'ENOTFOUND') {
+    if (e.code === 'ENOTFOUND') {
        console.log('Unable to connect')
-   }
+    } else {
+        console.log(e.message)
+    }
 });
